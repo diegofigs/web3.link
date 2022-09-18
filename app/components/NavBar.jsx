c6f89f1733f194605a0eb641dbbaf6c9b91856af
Christian <christian-game13@hotmail.com>
2022-09-18 06:42:43 -0400

    chore: :lipstick: Added Admin button styling, scroll auto when links overflow container. Done responsive .

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -23,7 +23,10 @@ export const NavBar = () => {
               </li>
               <li>Contact Us</li>
             </ul>
-            <div className="hidden md:flex ml-auto space-x-3">
+            <div className="hidden md:flex ml-auto space-x-4">
+              <button>
+                <Link href="/admin">Admin (provisional)</Link>{" "}
+              </button>
               <button>Login</button>
               <button className="pr-4">Sign Up</button>
             </div>
@@ -40,9 +43,9 @@ export const NavBar = () => {
           </div>
         </div>
       </div>
-      
-        <div className={!nav ? "hidden" : "md:hidden SlideLeft flex flex-col"}>
-            <ul className="mt-28 text-[32px]">
+
+      <div className={!nav ? "hidden" : "md:hidden SlideLeft flex flex-col"}>
+        <ul className="mt-28 text-[32px]">
           <li className="border-b-2 border-zinc-300 w-full  text-center">
             <Link href="/">Home</Link>
           </li>
@@ -50,7 +53,7 @@ export const NavBar = () => {
             <Link href="/about">About</Link>
           </li>
           <li className="border-b-2 border-zinc-300 w-full text-center">
-           <Link href ="/admin">Admin (provisional)</Link> 
+            <Link href="/admin">Admin (provisional)</Link>
           </li>
           <button className="border-b-2 border-zinc-300 px-8 w-screen text-black  md:mx-auto">
             Sign In
@@ -58,9 +61,8 @@ export const NavBar = () => {
           <button className="border-b-2 border-zinc-300 px-8 w-screen md:mx-auto">
             Sign Up
           </button>
-          </ul>
-        </div>
-     
+        </ul>
+      </div>
     </div>
   );
 };
--- app/pages/admin.js
+++ app/pages/admin.js
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import Link from "next/link";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
+import {TrashIcon, HomeIcon} from "@heroicons/react/24/outline/";
 
 export default function Admin() {
   const [links, setLinks] = useState({});
 
   const onDelete = (key) => {
-    let newLinks = { ...links};
+    let newLinks = { ...links };
     delete newLinks[key];
     setLinks(newLinks);
   };
@@ -32,62 +33,76 @@ export default function Admin() {
   const [prev, setPrev] = useState(false);
   const handleClickPrev = () => setPrev((p) => !p);
   return (
-    <div className="bg-red-200 w-screen h-screen flex flex-col lgs:flex-row relative flex-1">
-      <section className=" bg-white lgs:h-full lgs:w-[56px] h-[42px] flex"></section>
-      <div className="lgs:h-full lgs:w-[1000px] flex flex-col h-full">
-        <div className="lgs:h-[56px] lgs:w-screen flex lgs:flex-row flex-1 flex-col">
-          <div className="lgs:h-full lgs:w-1/2 flex h-[42px]">
+    <div className="bg-red-200 w-screen h-screen flex flex-col lgs:flex-row overflow-hidden">
+      <section className=" bg-white lgs:h-full lgs:w-[2.5%] h-[6%] flex justify-items-center flex-col">
+        <button>
+        <Link href="/"><HomeIcon className="w-[80%]"/></Link>
+        </button>
+      </section>
+      <div className="lgs:h-full lgs:w-[55%] flex flex-col h-[82%]">
+        <div className="lgs:h-[56px] lgs:w-screen flex lgs:flex-row flex-col">
+          <div className="lgs:h-full lgs:w-[50%] flex h-[42px]">
             <ul className="flex flex-row space-x-4 px-4 text-[24px]">
               <li>Links</li>
               <li>Appearance</li>
             </ul>
           </div>
-          <div className="lgs:h-full lgs:w-1/2 order-first lgs:order-last flex h-[42px} px-4">
+          <div className="lgs:h-full lgs:w-[45%] order-first lgs:order-last flex h-[42px} px-4 lgs:items-center lgs:justify-end ">
             <Link href="/">Mylink/fire</Link>
           </div>
         </div>
-        <div className="lgs:h-full lgs:w-screen flex lgs:flex-row h-full w-screen">
-          <div className="lgs:h-full lgs:w-[55%] flex w-screen justify-center lgs:justify-start">
-            <div className="mb-12 mx-4 w-screen ">
-              <div className="my-4">
-                <div className="mx-8 space-x-32 flex flex-row w-screen ">
-                  <button onClick={onAdd}>Add New Link</button>
-                  <button>Suggestion</button>
+        <div className="lgs:h-full lgs:w-screen flex lgs:flex-row h-full">
+          <div className="lgs:h-full lgs:w-[55%] flex justify-center lgs:justify-start">
+            <div className="flex-1 flex justify-center lgs:w-[60%] lgs:border-y-4 lgs:border-r-4 border-t-4 border-white">
+              <div className="my-4 flex-1">
+                <div className="space-x-24 flex mb-4 lgs:mb-0 flex-row w-screen lgs:w-full flex-1 justify-center lgs:space-x-18 lgs:mt-5">
+                  <button className="AddNew_Suggestion_Btn"onClick={onAdd}>Add New Link</button>
+                  <button className="AddNew_Suggestion_Btn">Suggestion</button>
                 </div>
-                <div className="flex w-screen mx-4 mt-2">
-                  <div>
-                    {Object.entries(links).map(([key, value]) => (
-                      <div key={key}>
-                        <div>
-                          <span>Name</span>
-                          <input
-                            type="text"
-                            value={value.name}
-                            onChange={(e) =>
-                              onUpdate(key, {
-                                ...value,
-                                name: e.target.value,
-                              })
-                            }
-                          />
-                          <span>Link</span>
-                          <input
-                            type="text"
-                            value={value.link}
-                            onChange={(e) =>
-                              onUpdate(key, {
-                                ...value,
-                                link: e.target.value,
-                              })
-                            }
-                          />
-                          <button onClick={() => onDelete(key)}>
-                            Delete Link
-                          </button>
-                        </div>
+                <div className="w-screen h-[83%] lgs:w-full lgs:h-[88%] lgs:mt-8 overflow-auto">
+                <div className="flex flex-1 w-screen flex-col lgs:w-full items-center">
+                  <div className="flex flex-1 w-[80%] lgs:w-full flex-col lgs:space-y-8  space-y-4 lgs:items-center">
+                  {Object.entries(links).map(([key, value]) => (
+                    <div
+                      key={key}
+                      className="flex flex-1 flex-col  w-full lgs:w-[60%] bg-white rounded-[1.1rem] border-white border-x-[1rem] border-y-[1rem]"
+                    >
+                      <input
+                        className="flex flex-0.5 w-full"
+                        type="text"
+                        placeholder="Title"
+                        value={value.name}
+                        onChange={(e) =>
+                          onUpdate(key, {
+                            ...value,
+                            name: e.target.value,
+                          })
+                        }
+                      />
+
+                      <input
+                        className="flex flex-0.5 w-full mt-1"
+                        type="text"
+                        placeholder="Link"
+                        value={value.link}
+                        onChange={(e) =>
+                          onUpdate(key, {
+                            ...value,
+                            link: e.target.value,
+                          })
+                        }
+                      />
+                      <div className="flex flex-1 self-end mt-1">
+                        <button
+                          onClick={() => onDelete(key)}
+                        >
+                          <TrashIcon className="w-4 lgs:w-6"/>
+                        </button>
                       </div>
-                    ))}
+                    </div>
+                  ))}
                   </div>
+                </div>
                 </div>
               </div>
             </div>
@@ -95,15 +110,15 @@ export default function Admin() {
           <div
             className={
               !prev
-                ? "lgs:h-full lgs:w-[45%] hidden lgs:inline"
-                : "SlideUp -translate-y-[122px] scale-y-[1.005]"
+                ? "lgs:h-full lgs:w-[43%] hidden lgs:inline border-t-4 border-white"
+                : "SlideUp -translate-y-[115px] scale-y-[1.005]"
             }
           >
             Preview div
           </div>
         </div>
       </div>
-      <div className=" lgs:hidden h-[85px] flex justify-center inset-x-0 bottom-0 z-10">
+      <div className=" lgs:hidden h-[10%] flex justify-center z-10">
         <button onClick={handleClickPrev}>Preview</button>
       </div>
     </div>
